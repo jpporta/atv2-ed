32e5cb43824c755bdd61b103a9a589fe9a5865be
João Pedro Porta <joao.pp3@puccampinas.edu.br>
2017-03-11 20:08:36 -0300

    Funcao 1 - ok | funcao 2 - preciso arrumar
    mudei algumas coisas na funcao 1, atualize a sua

---
+++ funcoes.c
@@ -0,0 +1,87 @@
+#include "funcoes.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//***************************************************************************************************
+// INSERIR DO ALUNO
+void inserirAluno(struct tipoAluno *i, struct tipoAluno *f) {
+        if (i == NULL) {
+                i = (struct tipoAluno *)malloc(sizeof(struct tipoAluno));
+                f = i;
+        } else {
+                f->prox = (struct tipoAluno *)malloc(sizeof(struct tipoAluno));
+                f = f->prox;
+        }
+        if ((int)f == -1) {
+                printf("\t---HEAP OVERFLOW----\nEnter para continuar\n");
+                while (getchar() != '\n') ;
+                getchar();
+        } else {
+                f->prox = NULL;
+                printf("\tNome:\n");
+                scanf("%s", (f->nome));
+                printf("\tRA:\n");
+                scanf("%ld", &(f->ra));
+                printf("\tNota:\n");
+                scanf("%f", &(f->nota));
+
+                printf("SUCESSO");
+                while (getchar() != '\n') ;
+                getchar();
+        }
+}
+//***************************************************************************************************
+// REMOVER DO ALUNO
+void removerCadastro(struct tipoAluno *i, struct tipoAluno *f) {
+        struct tipoAluno *antecessor = NULL, *dobby;
+        long int ra;
+        printf("\tRA a ser desmatriculado:\n");
+        scanf("%ld", &ra);
+        //antecessor = i;
+        if(i != NULL) {
+                if(i->ra == ra) {
+                        dobby = i;
+                        i = i->prox;
+                        free(dobby);
+                }
+                else{
+                        do {
+                                if(antecessor->prox == f) {
+                                        if(f->ra == ra) {
+                                                dobby = f;
+                                                f = antecessor;
+                                                free(dobby);
+                                                break;
+                                        }
+                                }else{
+                                        if((antecessor->prox)->ra == ra) {
+                                                dobby = antecessor->prox;
+                                                free(dobby);
+                                                break;
+                                        }
+                                }
+                                antecessor = antecessor->prox;
+                        } while(antecessor != NULL);
+                }
+        }
+        if(antecessor == NULL) {
+                printf("\tAluno ainda nao cadastrado\n");
+        }
+        else{
+                printf("\tSUCESSO\n");
+        }
+        while (getchar() != '\n') ;
+        getchar();
+}
+//***************************************************************************************************
+// BUSCAR POR RA DO ALUNO
+void buscarRA(struct tipoAluno *i, struct tipoAluno *f, long int ra) {
+}
+//***************************************************************************************************
+// BUSCAR POR NOME DO ALUNO
+void buscarNome(struct tipoAluno *i, struct tipoAluno *f, char *nome) {
+}
+//***************************************************************************************************
+// LISTAR TODOS
+void listar() {
+}
---
+++ main.c
@@ -0,0 +1,45 @@
+#include "funcoes.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// inicio do programa
+int main() {
+        int op;
+        struct tipoAluno *inicio = NULL, *ultimo = NULL;
+
+        do {
+                system("clear");
+                // MENU PRINCIPAL
+                printf("\tMenu:\n");
+                printf("\t     1- Inserir o cadastro de um aluno\n");
+                printf("\t     2- Remover o cadastro de um aluno\n");
+                printf("\t     3- Buscar aluno por RA\n");
+                printf("\t     4- Buscar aluno por Nome\n");
+                printf("\t     5- Exibir dados de todos os alunos cadastrados\n");
+                printf("\t     0- Sair\n");
+                scanf("%i", &op);
+                system("clear");
+                switch (op) {
+                case 1:
+                        inserirAluno(inicio, ultimo);
+                        break;
+                case 2:
+                        removerCadastro(inicio, ultimo);
+                        break;
+                case 3:
+                        buscarRA();
+                        break;
+                case 4:
+                        buscarNome();
+                        break;
+                case 5:
+                        listar();
+                        break;
+                case 0:
+                        break;
+                }
+        } while (op != 0);
+
+        return 0;
+}
